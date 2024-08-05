50d2b9c3ac4d49602bbd38f2df0cf1086e53e929
Shishir Halder <s.halder0108@gmail.com>
2024-08-05 09:51:20 +0600

    fix & feat: fix error status, receive tokens only from headers and refine jwt error handling

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,36 +2,50 @@ import { ApiError } from "../utils/api-error";
 import { asyncHandler } from "../utils/async-handler";
 import jwt from "jsonwebtoken";
 import { User } from "../db/models/user.model";
+import { Types } from "mongoose";
 
+// Middleware to verify JWT in the Authorization header of incoming requests
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
-    const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
+    // Extract the access token from the Authorization header and remove the 'Bearer ' prefix
+    const token = req.header("Authorization")?.replace("Bearer ", "");
 
+    // If no token is provided, throw an unauthorized error
     if (!token) {
-      throw new ApiError(401, "Unauthorized request");
+      throw new ApiError(401, "Unauthorized");
     }
 
+    // Verify the token using the secret key and decode it to extract user details
     const decodedToken = jwt.verify(
       token,
       process.env.ACCESS_TOKEN_SECRET!
-    ) as { _id: string };
+    ) as { _id: Types.ObjectId };
 
+    // Find the user by the _id obtained from the decoded token while excluding sensitive information
     const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
     );
 
+    // If no user is found, it indicates an invalid token
     if (!user) {
       throw new ApiError(401, "Invalid Access Token");
     }
 
+    // Attach the user object to the request object for use in subsequent middleware or route handlers
     req.user = user;
-    next();
+    next(); // Continue to the next middleware
   } catch (error: any) {
-    throw new ApiError(
-      error.statusCode || 401,
-      error?.message || "Invalid access token"
-    );
+    // Handle specific JWT errors based on their type
+    switch (error.name) {
+      case "TokenExpiredError": // Token has expired
+        throw new ApiError(401, "Access token expired");
+      case "JsonWebTokenError": // Token is malformed or invalid
+        throw new ApiError(400, "Invalid access token");
+      default: // Handle other types of errors
+        const statusCode = error.statusCode || 500;
+        const message =
+          error.message || "An error occurred during authentication";
+        throw new ApiError(statusCode, message);
+    }
   }
 });
